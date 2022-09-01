998b933f3cf5293dc934fda108e319c646971149
amyCuol <45844967+amyCuol@users.noreply.github.com>
2022-09-01 08:49:41 +0800

    Add UI layout on home page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Link, Outlet } from "react-router-dom";
+import { AiFillHome, AiOutlineHome } from "react-icons/ai";
+import { MdFavorite } from "react-icons/md";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+import styled from "styled-components";
 
 import {
   Container,
@@ -11,19 +15,32 @@ import {
 function App() {
   return (
     <>
-    <Container fluid className="h-100">
+    <Container fluid className="h-100 ">
       <Row className="h-90">
         <Col>
           <Outlet />
         </Col>
       </Row>
-      <Row className="h-10">
+      <Row className="h-10 bottomNav">
         <Col>
-          <Nav className="d-flex justify-content-center">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/pantry">Pantry</Nav.Link>
-            <Nav.Link as={Link} to="/favourites">Favourites</Nav.Link>
-            <Nav.Link as={Link} to="/shopping">Shopping</Nav.Link>
+          <Nav className="d-flex justify-content-center" >
+            <Nav.Link as={SLink} to="/">
+              <AiFillHome size={25}/>
+              Home
+              </Nav.Link>
+            <Nav.Link as={SLink} to="/pantry">
+              <AiOutlineHome size={25}/>
+              Pantry
+              </Nav.Link>
+            <Nav.Link as={SLink} to="/favourites">
+              <MdFavorite size={25}/>
+              Favourites
+              </Nav.Link>
+            <Nav.Link as={SLink} to="/shopping">
+              <RiShoppingCart2Fill size={25}/>
+              Shopping
+              </Nav.Link>
+
           </Nav>
         </Col>
       </Row>
@@ -31,5 +48,30 @@ function App() {
     </>
   );
 }
+// Styling nav links and icons using styled-components
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  svg{
+    color: white;
+  }
+  font-size:0.7em;
+  color: black;
+  &:hover {
+    transition: 0.1s ease-out;
+    color: #24BF82;
+    svg{
+      transition: 0.1s ease-out;
+      color: #24BF82;
+    }
+}
+  &.active{
+    color: black;
+    svg{ 
+      color: black;
+    }
+  }
+`;
 
 export default App;
---
+++ ui/src/components/Italian.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Italian() {
+  return (
+    <div>
+      <h3> Italian </h3>
+    </div>
+  )
+}
+
+export default Italian
